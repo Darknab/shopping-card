fadcad576a1ee0f1b5515c58d9ae529db1b3ce8c
Darknab <117382045+Darknab@users.noreply.github.com>
2024-05-14 05:58:53 +0100

    Write main content of the navvigation menu

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function Navigation() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><Link to="/">Awesome shop</Link></li>
+          <li><Link to="#">Men</Link></li>
+          <li><Link to="#">Women</Link></li>
+          <li><Link to="#">Jewelery</Link></li>
+          <li><Link to="#">Electronics</Link></li>
+          <li><Link to="#">Cart</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
