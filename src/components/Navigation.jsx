9d8670f4cd9933b4b025a3376738f9616d745270
Darknab <117382045+Darknab@users.noreply.github.com>
2024-05-19 05:35:13 +0100

    Convert Link element to HashLink element so the links can point to the diffrent sections of the Shop page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link"
 import { PropTypes } from "prop-types";
 
 export default function Navigation({ count }) {
@@ -7,10 +8,10 @@ export default function Navigation({ count }) {
       <nav>
         <ul className="nav-menu">
           <li><Link to="/">Awesome shop</Link></li>
-          <li><Link to="#">Men</Link></li>
-          <li><Link to="#">Women</Link></li>
-          <li><Link to="#">Jewelery</Link></li>
-          <li><Link to="#">Electronics</Link></li>
+          <li><HashLink to="/shop#men">Men</HashLink></li>
+          <li><HashLink to="/shop#women">Women</HashLink></li>
+          <li><HashLink to="/shop#jewelery">Jewelery</HashLink></li>
+          <li><HashLink to="/shop#electronics">Electronics</HashLink></li>
           <li><Link to="/cart">Cart <span>{count}</span></Link></li>
         </ul>
       </nav>
