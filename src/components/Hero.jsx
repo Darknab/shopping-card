9106e40401f684a96132df3ad7d7e03d8050bb6f
Darknab <117382045+Darknab@users.noreply.github.com>
2024-05-19 10:04:04 +0100

    Move the contziner inside the section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 export default function Hero() {
   return (
-    <section className="container" id="hero">
+    <section id="hero">
+      <div className="container">
       <div className="hero-content">
         <h1 className="site-title">Awesome Shop</h1>
         <Link to="/shop">
           <button className="shop">Shop now</button>
         </Link>
+      </div>
       </div>
     </section>
   );
