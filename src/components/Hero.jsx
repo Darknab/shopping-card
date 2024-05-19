9d031a7758e951ccc9b94dbe19552537e68d7477
Darknab <117382045+Darknab@users.noreply.github.com>
2024-05-19 09:23:30 +0100

    Add class names and ids for styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function Hero() {
   return (
-    <section id="hero">
-      <h1>Awesome Shop</h1>
-      <Link to="/shop">
-        <button>Shop now</button>
-      </Link>
+    <section className="container" id="hero">
+      <div className="hero-content">
+        <h1 className="site-title">Awesome Shop</h1>
+        <Link to="/shop">
+          <button className="shop">Shop now</button>
+        </Link>
+      </div>
     </section>
   );
 }
