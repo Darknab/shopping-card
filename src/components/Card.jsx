b3aa13faf36926f22e7b0dd8ed7ee72225655062
Darknab <117382045+Darknab@users.noreply.github.com>
2024-05-21 10:11:48 +0100

    Add buttons to increment/decrement quantity . Add classNames to help styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,40 @@
 import { PropTypes } from "prop-types";
+import { useState } from "react";
 
 export default function Card({title, price, image, addToCart }) {
+  const [ quantity, setQuantity ] = useState(1);
+
+  function handleIncrement(e) {
+    e.preventDefault();
+    setQuantity(quantity + 1);
+  }
+
+  function handleDecrement(e) {
+    e.preventDefault();
+    setQuantity(quantity - 1);
+  }
+
+  function handleChange(e) {
+    setQuantity(e.target.value);
+  }
+
   return (
     <div className="card">
       <h3>{title}</h3>
-      <img src={image} alt={title} />
-      <p>{price}</p>
-      <form onSubmit={(e) => addToCart(e, image, title, price)}>
-        <label>
-          <input type="number" name="quantity" defaultValue={1} />
-        </label>
-        <button type="submit">Add to Cart</button>
+      <div className="image-container">
+        <img src={image} alt={title} width={150} />
+      </div>
+      <p className="price"><span>$</span>{price.toFixed(2)}</p>
+      <form onSubmit={(e) => addToCart(e, image, title, price, quantity)}>
+        <div className="quantity-wrapper">
+          <label>
+            <input className="quantity" type="number" name="quantity" min={1} value={quantity} onChange={(e) => handleChange(e)} />
+            <button className="control increment" onClick={handleIncrement}>+</button>
+            <button className="control decrement" onClick={handleDecrement}>-</button>
+          </label>
+        </div>
+
+        <button className="add-to-cart" type="submit">Add to Cart</button>
       </form>
     </div>
   );
