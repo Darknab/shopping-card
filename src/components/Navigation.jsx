e46a92c0d8649d5daf29db4956b19b7779a34fbd
Darknab <117382045+Darknab@users.noreply.github.com>
2024-05-28 10:38:15 +0100

    Add the hamburger menu control

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,21 +3,31 @@ import { HashLink } from "react-router-hash-link"
 import { PropTypes } from "prop-types";
 import Icon from '@mdi/react';
 import { mdiCartOutline } from '@mdi/js';
+import Hamburger from "./Hamburger";
+import { useState } from 'react';
 
 export default function Navigation({ count }) {
+  const [ isMenuDisplayed, setIsMenuDisplayed ] = useState(false);
+
+  function handleClick() {
+    setIsMenuDisplayed(! isMenuDisplayed);
+  }
   return (
-    <header>
-      <nav>
-        <ul className="nav-menu">
-          <li className="nav-link" id="home-link"><Link to="/">Awesome shop</Link></li>
-          <li className="nav-link"><HashLink to="/shop#men">Men</HashLink></li>
-          <li className="nav-link"><HashLink to="/shop#women">Women</HashLink></li>
-          <li className="nav-link"><HashLink to="/shop#jewelery">Jewelery</HashLink></li>
-          <li className="nav-link"><HashLink to="/shop#electronics">Electronics</HashLink></li>
-          <li id="cart-link"><Link to="/cart"><Icon className="icon" path={mdiCartOutline} size={1} /> <sub className="count" id={count === 0 ? "empty" : "full"}>{count}</sub></Link></li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <Hamburger onClick={handleClick} isMenuDisplayed={isMenuDisplayed} />
+      <header className={isMenuDisplayed ? "active" : null} >
+        <nav >
+          <ul className="nav-menu">
+            <li className="nav-link" id="home-link"><Link to="/">Awesome shop</Link></li>
+            <li className="nav-link"><HashLink to="/shop#men">Men</HashLink></li>
+            <li className="nav-link"><HashLink to="/shop#women">Women</HashLink></li>
+            <li className="nav-link"><HashLink to="/shop#jewelery">Jewelery</HashLink></li>
+            <li className="nav-link"><HashLink to="/shop#electronics">Electronics</HashLink></li>
+            <li id="cart-link"><Link to="/cart"><Icon className="icon" path={mdiCartOutline} size={1} /> <sub className="count" id={count === 0 ? "empty" : "full"}>{count}</sub></Link></li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
 
