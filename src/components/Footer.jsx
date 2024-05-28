3ae4d1120ac93133285b80ea04f2c7f2214e22ea
Darknab <117382045+Darknab@users.noreply.github.com>
2024-05-28 19:35:02 +0100

    Create a footer with credentials

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+export default function Footer () {
+  return (
+    <footer>
+      <p>Made by <a href="https://github.com/Darknab" target="_blank">Nabil Negadi</a></p>
+      <p>
+        <span>
+          Photo by <a href="https://unsplash.com/@mohammadsalman?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash" target="_blank">Md Salman</a> on <a href="https://unsplash.com/photos/two-black-crew-neck-shirt-iiqg2xbmvk0?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+        </span>
+        <span>
+          Products from <a href="https://fakestoreapi.com/" target="_blank">fakestore API</a>
+        </span>
+        <span>
+          Icons from <a href="https://pictogrammers.com/" target="_blank">pictogrammers</a>
+        </span>
+      </p>
+    </footer>
+
+  );
+}
